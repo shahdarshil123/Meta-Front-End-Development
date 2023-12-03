46b14f69dc7a249fc8d045ded14eec5e297cdc99
shahdarshilr <shah.darshilr@northeastern.edu>
2023-12-02 21:51:06 -0500

    Course 2 started

---
+++ Course2/Week1/Excercise1/excercise1.js
@@ -0,0 +1,10 @@
+var result = 26;
+if(result<25){
+    console.log("Number less than 25");
+}
+else if(result == 25){
+    console.log("Number is 25");
+}
+else{
+    console.log("Number higher than 25");
+}
